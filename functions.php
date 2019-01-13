d42964606f40a0fbc88d9ef88b0f78a5bd065159
Maryann Reissig <mreissig@sjfc.edu>
2019-01-13 15:24:37 -0500

    Initial commit

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php 
+
+//place lines below in your funtions.php file to load the modules into Divi
+
+include(get_stylesheet_directory() . '/custom-modules/trainer-blog.php');
+include(get_stylesheet_directory() . '/custom-modules/member-blog.php');
